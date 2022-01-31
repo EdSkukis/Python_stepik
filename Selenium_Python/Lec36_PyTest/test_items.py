b25afb1634e4951f80a3f3186e8d4de0722423db
EdSkukis <97447328+EdSkukis@users.noreply.github.com>
2022-01-31 12:32:34 +0200

    Add files via upload
    Задания на проверку

---
+++ Selenium_Python/Lec36_PyTest/test_items.py
@@ -0,0 +1,15 @@
+# pytest --language=es test_items.py
+import time
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    #time.sleep(5)
+    buttom = browser.find_element(By.CSS_SELECTOR,'button.btn')
+    assert NoSuchElementException, "No create class button"
+
